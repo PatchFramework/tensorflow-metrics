a3b197580f745144819e91bbfaad4f8ace835265
PatchFramework <dominic.viola@online.de>
2021-09-03 14:23:16 +0200

    split eval and trainign metrics

--- get_metrics_from_console_logs.py
+++ get_metrics_from_console_logs.py
@@ -1,6 +1,9 @@
 import os 
 import re
 import argparse
+import numpy as np
+import pandas as pd
+import logging
 
 class MetricsExtractor():
     def __init__(self, args):
@@ -10,6 +13,9 @@ class MetricsExtractor():
         self.file = args.file
         self.debug = args.debug
         self.is_print = args.is_print
+        self.start_iteration = args.start_iter
+        self.train_metrics = {}
+        self.eval_metrics = {}
 
     def cast_to(self, value, type):
         """
@@ -106,21 +112,58 @@ class MetricsExtractor():
     
     def extract(self):
         # Regex we want to collect into a list
-        eval_time = self.get_regex_group('^# Eval time: \[(.*)\]s$', "float")
-        eval_avg_return = self.get_regex_group('^# Eval average return: (.*)$', "float")
+        
+        # Still need to collect the correct iteration where the eval metrics were extracted
+        #self.eval_metrics["iteration"] = self.get_regex_group()
+        self.eval_metrics["eval_time"] = self.get_regex_group('^# Eval time: \[(.*)\]s$', "float")
+        self.eval_metrics["eval_avg_return"] = self.get_regex_group('^# Eval average return: (.*)$', "float")
+        
+        self.train_metrics["iteration"] = self.get_regex_group('^ ITERATION (.*)$', "int")
+        self.train_metrics["env_steps"] = self.get_regex_group('^# Env\. Steps:   (.*)$', "int")
+        self.train_metrics["train_steps"] = self.get_regex_group('^# Train Steps:  (.*)$', "int")
+        self.train_metrics["collect_time"] = self.get_regex_group('^# Collect time: \[(.*)\]s$', "float")
+        self.train_metrics["train_time"] = self.get_regex_group('^# Train time:   \[(.*)\]s$', "float")
 
-        env_steps = self.get_regex_group('^# Env\. Steps:   (.*)$', "int")
-        train_steps = self.get_regex_group('^# Train Steps:  (.*)$', "int")
-        collect_time = self.get_regex_group('^# Collect time: \[(.*)\]s$', "float")
-        train_time = self.get_regex_group('^# Train time:   \[(.*)\]s$', "float")
+        if self.is_print:  
+            # print the values in the dict
+            print("########## Training metrics ##########")
+            for key, value in self.train_metrics.items():
+                print(f"\n{key}:\n{value}")  
+            
+            print("########## Eval metrics ##########")
+            for key, value in self.eval_metrics.items():
+                print(f"\n{key}:\n{value}")  
 
-        if self.is_print:    
-            print("eval_time\n", eval_time)
-            print("eval_avg_return\n", eval_avg_return)
-            print("env_steps\n", env_steps)
-            print("train_steps\n", train_steps)
-            print("collect_time\n", collect_time)
-            print("train_time\n", train_time)
+    def metrics_df(self):
+        assert self.train_metrics != {}, "No training metrics have been collected yet"
+        assert self.eval_metrics != {}, "There are training metrics, but no eval metrics have been collected yet"
+
+        try:
+            train_df = pd.DataFrame(data=self.train_metrics)
+            eval_df = pd.DataFrame(data=self.eval_metrics)
+        except:
+            logging.error(
+                """
+                Dataframe of training metrics could not be created.
+                This is probably due to incomplete information about an iteration in the beginning or the end of the file.
+                Make sure there are only blocks that all belong to one iteration, like so:
+                ------------------------------------------------------------------------
+                ITERATION 123
+                ------------------------------------------------------------------------
+                # Episodes:     1234
+                # Env. Steps:   1234
+                # Train Steps:  1234
+
+                # Collect time: [12.34]s
+                # Train time:   [12.34]s
+                # TOTAL:        [12.34]s
+                """)
+        print(train_df)
+        print(eval_df)
+
+    def get_metrics_dicts(self):
+        return self.train_metrics, self.eval_metrics
+
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -131,8 +174,12 @@ if __name__=='__main__':
     parser.set_defaults(debug=False)
     parser.add_argument("-p", "--print", dest='is_print', action='store_true', help="Set this flag if you want to get the metrics printed out to the console. Default: false")
     parser.set_defaults(is_print=False)
+    parser.add_argument("-s", "--start_iter", default=0, type=int, help="The first iteration that is provided in the console output. Might be usefull if you didn't start a fresh training. Default: 0")
     args = parser.parse_args()
 
     metr_ex = MetricsExtractor(args)
     metr_ex.extract()
+    metr_ex.metrics_df()
+
+    #print(metr_ex.get_metrics_dict())
     
